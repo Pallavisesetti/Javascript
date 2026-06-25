30d07a6eb240764fe442c4954dfd00e9579d9a00
Pallavi <pallavisesetti89@gmail.com>
2026-06-25 18:49:06 +0530

    Addition,Number,parseInt,parseFloat,js,Subtraction

---
+++ Numbers/NumberParseIntParseFloat.js
@@ -0,0 +1,21 @@
+
+    
+        function onClickCheckingnumber(){
+        debugger;
+        var a=document.getElementById("txtCheckNumber").value;
+        number=Number(a);
+        alert(number);
+    }
+     function onClickCheckingParseInt(){
+        debugger;
+        var a=document.getElementById("txtCheckNumber").value;
+        number=parseInt(a);
+        alert(number);
+    }
+     function onClickCheckingParseFloat(){
+        debugger;
+        var a=document.getElementById("txtCheckNumber").value;
+        number=parseFloat(a);
+        alert(number);
+    }
+     
